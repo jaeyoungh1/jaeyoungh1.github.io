264a84a49e8aec030dba3084f11610c72fbcb4a9
Jae Hwang <jaeyoung1997@gmail.com>
2022-11-10 21:20:45 -0800

    added nav menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,48 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './Navbar.css'
 import menu from '../icons/menu.svg'
-import logo from '../icons/logo.svg'
-
 
 export default function Navbar() {
 
     const [openMenu, setOpenMenu] = useState(false)
+    function scrollTo(str) {
+        let ele = document.querySelector(`.${str}`)
+        console.log("ELE", ele)
+        if (str === 'technologies') {
+            ele.scrollIntoView({ behavior: 'smooth', block: 'center'})
+
+        } else {
+
+            ele.scrollIntoView({ behavior: 'smooth' })
+        }
+    }
 
     return (
         <>
-        <nav>
-            <div className='logo'>
-                {/* <img className='logo' alt='menu' src={logo} /> */}
-                JH
-
-            </div>
-            <div className='my-info'>
-                <div className='name'>
-                    Jae Hwang
+            <nav>
+                <div className='logo'>
+                    JH
                 </div>
-                <div className='title'>
-                    Full-Stack Software Engineer
+                <div className='my-info'>
+                    <div className='name'>
+                        Jae Hwang
+                    </div>
+                    <div className='title'>
+                        Full-Stack Software Engineer
+                    </div>
                 </div>
-            </div>
-            <div className='left'>
-                <img className='menu-icon' alt='menu' src={menu}
-                onClick={() => setOpenMenu(!openMenu)}/>
-            </div>
-            {openMenu && (
-                <div className='menu' >
-                    <div>About Me</div>
-                    <div></div>
+                <div className='left'>
+                    <img className='menu-icon' alt='menu' src={menu}
+                        onClick={() => setOpenMenu(!openMenu)} />
                 </div>
-            )}
-        </nav>
+                {openMenu && (
+                    <div className='menu' >
+                        <div onClick={() => scrollTo('technologies')}>Technologies</div>
+                        <div onClick={() => scrollTo('projects')}>Projects</div>
+                        <div onClick={() => scrollTo('contact')}>Contact Me</div>
+                    </div>
+                )}
+            </nav>
         </>
     )
 }
