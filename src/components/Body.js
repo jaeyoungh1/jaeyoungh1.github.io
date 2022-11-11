270b234ea2eab3e0d9af823983c9533a193d1eae
Jae Hwang <jaeyoung1997@gmail.com>
2022-11-10 18:50:57 -0800

    about me

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,47 @@
 import React from 'react'
 import water from '../icons/water2.mp4'
+import me from '../icons/me2.jpg'
+import './Body.css'
 
 export default function Body() {
 
     return (
-        <div className='body'>
-            <video src={water} autoPlay loop muted playsInline />
-        </div>
+        <>
+            <div className='body'>
+                <video src={water} autoPlay loop muted playsInline />
+            </div>
+            <div className='aboutme'>
+                <img className='profpic' alt='me' src={me}/>
+                <p className='desc'>
+                    I'm a full-stack software engineer based in the South Bay, who's passionate about 
+                    web development. I love the relationship between function and aesthetic, and finding creative solutions
+                    to problems.
+                    
+                    I'm a team players who's always down to take on the challenge of struggling through an interesting puzzle.
+                    I also have a personal passion for coffee, cats, and corny jokes 🌽 .
+                </p>
+            </div>
+            <div className='technologies'>
+                <div className='div-title'>Technologies</div>
+                <div className='tech-icons'>
+                    <img alt='git' height='80' width='80' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain-wordmark.svg" />
+                    <img alt='express' height='80' width='80' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg" />
+                    <img alt='heroku' height='80' width='80'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-original.svg" />
+                    <img alt='css3' height='80' width='80' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg" />
+                    <img alt='docker' height='80' width='80' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain-wordmark.svg" />
+                    <img alt='html5' height='80' width='80' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg" />
+                    <img alt='flask' height='80' width='80' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flask/flask-original-wordmark.svg" />
+                    <img alt='react' height='80' width='80' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" />
+                    <img alt='redux' height='80' width='80' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" />
+                    <img alt='sequelize' height='80' width='80' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-plain-wordmark.svg" />
+                    <img alt='sqlite' height='80' width='80' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-plain-wordmark.svg" />
+                    <img alt='sqlalchemy' height='80' width='80' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlalchemy/sqlalchemy-original-wordmark.svg" />
+                    <img alt='postgresql' height='80' width='80'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-plain-wordmark.svg" />
+                    <img alt='python' height='80' width='80' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" />
+                    <img alt='javascript' height='80' width='80' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+                    <img alt='nodejs' height='80' width='80' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain.svg" />
+                          </div>
+            </div>
+        </>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Navbar.css'
 import menu from '../icons/menu.svg'
 import logo from '../icons/logo.svg'
@@ -6,14 +6,17 @@ import logo from '../icons/logo.svg'
 
 export default function Navbar() {
 
+    const [openMenu, setOpenMenu] = useState(false)
+
     return (
+        <>
         <nav>
-            <div className='right'>
-                <img className='logo' alt='menu' src={logo} />
+            <div className='logo'>
+                {/* <img className='logo' alt='menu' src={logo} /> */}
+                JH
 
             </div>
-            <div>
-
+            <div className='my-info'>
                 <div className='name'>
                     Jae Hwang
                 </div>
@@ -22,9 +25,16 @@ export default function Navbar() {
                 </div>
             </div>
             <div className='left'>
-                <img className='menu' alt='menu' src={menu} />
+                <img className='menu-icon' alt='menu' src={menu}
+                onClick={() => setOpenMenu(!openMenu)}/>
             </div>
-
+            {openMenu && (
+                <div className='menu' >
+                    <div>About Me</div>
+                    <div></div>
+                </div>
+            )}
         </nav>
+        </>
     )
 }
